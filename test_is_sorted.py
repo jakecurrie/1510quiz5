1848edc24b5aa7bde53a77ccfdc315079faa050b
Jacob Currie <jcurrie42@my.bcit.ca>
2023-02-07 14:21:20 -0800

    update test function names in test_is_sorted.py

--- test_is_sorted.py
+++ test_is_sorted.py
@@ -4,10 +4,10 @@ from is_sorted import is_sorted
 
 
 class Test(TestCase):
-    def test_ascending_positive(self):
+    def test_ascending_values(self):
         self.assertTrue(is_sorted([1, 2, 3]))
 
-    def test_descending_positive(self):
+    def test_descending_values(self):
         self.assertFalse(is_sorted([3, 2, 1]))
 
     def test_empty_list(self):
